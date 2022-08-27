198561079493758877c02af937938b56ca4149eb
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-27 19:11:02 -0400

    Added more err type to query

--- src/PolyJuka.Errors/ErrorDefinitons.cs
+++ src/PolyJuka.Errors/ErrorDefinitons.cs
@@ -13,6 +13,24 @@ namespace PolyJuka.Errors
                 string ERFND_EXAMPLE = "[fuchsia]fx [/][skyblue2]SomeFunction[/]() {\n   [red3]ret[/][skyblue2]1+1[/];\n}\n[underline red]SomeOtherFunction();[/]";
                 table = createPanel(ERROR_CODE, ERFND_EXAMPLE, ERFND);
             }
+            if (ERROR_CODE == "ERSIF")
+            {
+                string ERFND = "Error type: String type is invalid";
+                string ERFND_EXAMPLE = "[teal]string[/] fiz = [red bold underline]False[/]";
+                table = createPanel(ERROR_CODE, ERFND_EXAMPLE, ERFND);
+            }
+            if (ERROR_CODE == "ERVTI")
+            {
+                string ERFND = "Error type: Variable type invalid";
+                string ERFND_EXAMPLE = "[bold red underline]SOME_VARIABLE_TYPE[/] fiz;";
+                table = createPanel(ERROR_CODE, ERFND_EXAMPLE, ERFND);
+            }
+            if (ERROR_CODE == "ERCNC")
+            {
+                string ERFND = "Error type: Can not convert to type";
+                string ERFND_EXAMPLE = "[teal bold]console[/].[aqua]out[/].[purple]print[/]([bold red underline]22[/]);";
+                table = createPanel(ERROR_CODE, ERFND_EXAMPLE, ERFND);
+            }
             return table;
         }
         public static Table createPanel(string ERROR_CODE, string ERROR_EXAMPLE, string ERROR_DEFINITION)
