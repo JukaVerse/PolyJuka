b5131990863fd4dd1dd865166aaefe32469540f1
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-27 23:35:55 -0400

    Main runtime

---
+++ src/PolyJuka.Core/shell.cs
@@ -0,0 +1,53 @@
+﻿using Spectre.Console;
+
+namespace PolyJuka.Core
+{
+    public class PolyJuka
+    {
+        public class Shell
+        {
+            public static string NewLineBug(int line)
+            {
+                var res = string.Empty;
+                var lineData = AnsiConsole.Prompt(
+                     new TextPrompt<string>("[bold deeppink4]{" + line.ToString() + "}[/] [bold deeppink4]P[/][bold purple4]o[/][bold purple4_1]l[/][bold purple3]y[/] [bold blueviolet]>[/] ")
+                    .PromptStyle("silver")
+                    .AllowEmpty()
+                    .ValidationErrorMessage("Parser Error.")
+                    );
+                res = lineData;
+                if(lineData=="run();")
+                {
+                    line = 0;
+
+                    AnsiConsole.Write(new Rule("\n\n[green bold]Output[/]\n\n").LeftAligned().RoundedBorder().Border(border: BoxBorder.Double));
+                    
+                }
+                if (lineData == "exit();")
+                {
+                    AnsiConsole.Write(new Markup("[bold red]Terminating interactive shell...[/]"));
+                    Environment.Exit(0);
+                }
+                return lineData;
+            }
+            public static void New()
+            {
+                AnsiConsole.Write(new Markup("\n[grey23 dim]PolyJuka interactive command shell - v0.0.001[/]" +
+                    "\n[grey23 dim]You can use \"[/][grey23 underline bold]exit();" +
+                    "[/][grey23 dim]\" to quit the proccess or \"[/]" +
+                    "[grey23 underline bold]run();[/][grey23 dim]\" to execute[/]\n"));
+
+                int line = 1;
+                var lineData = NewLineBug(line);
+                while (line != 0)
+                {
+                    if (lineData != "run();")
+                    {
+                        line++;
+                        NewLineBug(line);
+                    }
+                }
+            }
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Diagnostics;
+using PolyJuka.Core;
 namespace PolyJuka.Runtime
 {
     public class Runtime
@@ -22,6 +23,7 @@ namespace PolyJuka.Runtime
                     .Mode(mode: SelectionMode.Independent)
                     .AddChoices(new[]
                     {
+                    "[darkslategray2 bold]Open PolyJuka Interactive Shell[/]",
                     "[darkslategray2 bold]New PolyJuka Project[/]",
                     "[darkslategray2 bold]Run PolyJuka Project[/]",
                     "[darkslategray2 bold]Help[/]",
@@ -30,6 +32,11 @@ namespace PolyJuka.Runtime
 
                     }));
 
+                if (Options == "[darkslategray2 bold]Open PolyJuka Interactive Shell[/]")
+                {
+
+                    Core.PolyJuka.Shell.New();
+                }
                 if (Options == "[darkslategray2 bold]Support[/]")
                 {
                     Process.Start("https://jukalang.com");
@@ -82,47 +89,50 @@ namespace PolyJuka.Runtime
                     //AnsiConsole.Write(new Markup( PolyJuka.Errors.ErrorBuilder.Build(Errors.ErrorTypes.SYNTAX_ERROR, Errors.ErrorCodes.VTI, 1, 5, "main.pjka")));
                 }
             }
-            if (args[0] == "dict" && args.Length == 1)
+            if (args.Length == 1)
             {
-                var error_code = AnsiConsole.Ask<string>("What is your [red bold]Error Code[/]?");
-                AnsiConsole.Write(PolyJuka.Errors.ErrorDefinitons.ErrorLookup(error_code));
-            }
-            if (args[0] == "help")
-            {
-                var table = new Table().LeftAligned();
+                if (args[0] == "dict")
+                {
+                    var error_code = AnsiConsole.Ask<string>("What is your [red bold]Error Code[/]?");
+                    AnsiConsole.Write(PolyJuka.Errors.ErrorDefinitons.ErrorLookup(error_code));
+                }
+                if (args[0] == "help")
+                {
+                    var table = new Table().LeftAligned();
 
-                AnsiConsole.Live(table)
-                    .Start(ctx =>
-                    {
-                        table.Border = TableBorder.Simple;
-                        table.Collapse();
-                        table.AddColumn("[skyblue1 bold]Command[/]");
-                        ctx.Refresh();
-                        Thread.Sleep(500);
-                        table.AddColumn("[skyblue1 bold]Usage[/]");
-                        ctx.Refresh();
-                        Thread.Sleep(500);
-                        table.AddColumn("[skyblue1 bold]Explanation[/]");
-                        ctx.Refresh();
-                        Thread.Sleep(500);
-                        table.AddRow("[lightgreen_1 bold]Run[/]", "[orange1 bold]poly[/] [red1 bold]run[/] [underline green bold]<FILE>[/][seagreen3].pjka[/]", "[bold blue]Run's a PolyJuka file through the interperter[/]");
-                        ctx.Refresh();
-                        Thread.Sleep(200);
-                        table.AddEmptyRow();
-                        table.AddRow("[lightgreen_1 bold]Build[/]", "[orange1 bold]poly[/] [red1 bold]build[/] [underline green bold]<BUILD_CONFIG_FILE>[/] [italic yellow]-o <OUTPUT_DIRECTORY>[/]", "[bold blue]Build's your PolyJuka project[/]");
-                        ctx.Refresh();
-                        Thread.Sleep(200);
-                        table.AddEmptyRow();
-                        table.AddRow("[lightgreen_1 bold]Dict[/]", "[orange1 bold]poly[/] [red1 bold]dict[/] [underline green bold]<ERROR_CODE>[/]", "[bold blue]Gives you an explentation on a certain error[/]");
-                        ctx.Refresh();
-                        Thread.Sleep(200);
-                        table.AddEmptyRow();
-                        table.AddRow("[lightgreen_1 bold]Initalize[/]", "[orange1 bold]poly[/] [red1 bold]init[/]", "[bold blue]Auto generates a build file[/]");
-                        ctx.Refresh();
-                        Thread.Sleep(200);
-                        table.AddEmptyRow();
+                    AnsiConsole.Live(table)
+                        .Start(ctx =>
+                        {
+                            table.Border = TableBorder.Simple;
+                            table.Collapse();
+                            table.AddColumn("[skyblue1 bold]Command[/]");
+                            ctx.Refresh();
+                            Thread.Sleep(500);
+                            table.AddColumn("[skyblue1 bold]Usage[/]");
+                            ctx.Refresh();
+                            Thread.Sleep(500);
+                            table.AddColumn("[skyblue1 bold]Explanation[/]");
+                            ctx.Refresh();
+                            Thread.Sleep(500);
+                            table.AddRow("[lightgreen_1 bold]Run[/]", "[orange1 bold]poly[/] [red1 bold]run[/] [underline green bold]<FILE>[/][seagreen3].pjka[/]", "[bold blue]Run's a PolyJuka file through the interperter[/]");
+                            ctx.Refresh();
+                            Thread.Sleep(200);
+                            table.AddEmptyRow();
+                            table.AddRow("[lightgreen_1 bold]Build[/]", "[orange1 bold]poly[/] [red1 bold]build[/] [underline green bold]<BUILD_CONFIG_FILE>[/] [italic yellow]-o <OUTPUT_DIRECTORY>[/]", "[bold blue]Build's your PolyJuka project[/]");
+                            ctx.Refresh();
+                            Thread.Sleep(200);
+                            table.AddEmptyRow();
+                            table.AddRow("[lightgreen_1 bold]Dict[/]", "[orange1 bold]poly[/] [red1 bold]dict[/] [underline green bold]<ERROR_CODE>[/]", "[bold blue]Gives you an explentation on a certain error[/]");
+                            ctx.Refresh();
+                            Thread.Sleep(200);
+                            table.AddEmptyRow();
+                            table.AddRow("[lightgreen_1 bold]Initalize[/]", "[orange1 bold]poly[/] [red1 bold]init[/]", "[bold blue]Auto generates a build file[/]");
+                            ctx.Refresh();
+                            Thread.Sleep(200);
+                            table.AddEmptyRow();
 
-                    });
+                        });
+                }
             }
         }
     }
